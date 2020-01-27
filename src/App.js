08482768496cc700da92ec499ddb8738dc5afc11
Abdu Qnauy <abduqnauy@gmail.com>
2020-01-27 15:34:05 +0200

    stream-list is done

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const App = props => {
         <>
           <Header />
           <Route path="/" exact component={StreamLits} />
+          <Route path="/streams" exact component={StreamLits} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
           <Route path="/streams/delete" exact component={StreamDelete} />
