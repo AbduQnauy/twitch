2f6abe487e7feacd84018a40d22eb15fec7bbb77
Abdu Qnauy <abduqnauy@gmail.com>
2020-01-29 08:16:15 +0200

    create stream & edit stream is done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Router, Route } from "react-router-dom";
+import history from "./history";
 import StreamCreate from "./components/streams/StreamCreate.Component";
 import StreamDelete from "./components/streams/StreamDelete.Component";
 import StreamEdit from "./components/streams/StreamEdit.Component";
@@ -9,17 +10,17 @@ import Header from "./components/Header";
 const App = props => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <>
           <Header />
           <Route path="/" exact component={StreamLits} />
           <Route path="/streams" exact component={StreamLits} />
           <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
           <Route path="/streams/delete" exact component={StreamDelete} />
           <Route path="/streams/show" exact component={StreamShow} />
         </>
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
