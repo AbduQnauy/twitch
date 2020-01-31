4c2b61822962be411ba84ed370e860037db6e310
Abdu Qnauy <abduqnauy@gmail.com>
2020-01-31 13:54:08 +0200

    full CRUD operations front-end application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import history from "./history";
 import StreamCreate from "./components/streams/StreamCreate.Component";
 import StreamDelete from "./components/streams/StreamDelete.Component";
@@ -11,15 +11,15 @@ const App = props => {
   return (
     <div className="ui container">
       <Router history={history}>
-        <>
-          <Header />
+        <Header />
+        <Switch>
           <Route path="/" exact component={StreamLits} />
           <Route path="/streams" exact component={StreamLits} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit/:id" exact component={StreamEdit} />
           <Route path="/streams/delete/:id" exact component={StreamDelete} />
-          <Route path="/streams/show" exact component={StreamShow} />
-        </>
+          <Route path="/streams/:id" exact component={StreamShow} />
+        </Switch>
       </Router>
     </div>
   );
